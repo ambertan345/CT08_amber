2cd802aeb767f19ff4bd25350fc9fe87bf9cc030
ambertan345 <ambertan2011@yahoo.com.sg>
2025-07-20 03:08:24 +0000

    Jul 20, 2025, 3:08 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -16,4 +16,21 @@ function setup() {
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
 
+  createCanvas(600, 400);//length and width of your canvas
+  background(220);//
+}
+
+function draw(){
+  fill(255, 0, 0); //rgb
+  ellipse(300, 100, 100, 100); //x-axis, y-axis, size
+
+}
+
+function draw(){
+  fill(255, 0, 0);
+  ellipse(300, 100, 100, 100);
+  fill(247, 255, 5);
+  ellipse(300, 200, 100, 100);
+  fill(5, 255, 155);
+  ellipse(300, 300, 100, 100);
 }
