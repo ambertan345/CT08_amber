f1d0b642e7bd0443d148ae46602dec8c5846f5ad
ambertan345 <ambertan2011@yahoo.com.sg>
2025-08-03 02:58:59 +0000

    Aug 3, 2025, 2:58 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -1,3 +1,4 @@
+let sum;
 function setup() {
   // write your codes here
 
@@ -16,9 +17,79 @@ function setup() {
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
 
-  createCanvas(600, 400);//length and width of your canvas
-  background(220);//
+  // createCanvas(600, 400);//length and width of your canvas
+  // background(220);//
+createCanvas(600, 400);
+background(220);
+
+console.log("Hello!");
+textSize(16);
+fill(0);
+
+let a = 5;
+let b = 10;
+sum = a + b;
+let product = a * b;
+console.log("Sum of a and b: ", sum);
+console.log("Product of a and b: ". product);
+
+base = 10;
+height = 5;
+let area = base * height * 0.5;
+console.log("Area of triangle: ", area);
+
+fill(0);
+text(area, 100, 100)
+
+
+for (let i = 10, i <= 20; i++) {
+  console.log("Counting..." + i)
 }
+
+for (let i = 20, i <= 1; i--) {
+  console.log("Counting..." + i)
+}
+
+for (let i = 3, i <= 46; i+3) {
+  console.log("Counting..." + i)
+}
+
+for (let i + 2, i <= 20; i+=2) {
+  console.log("Counting..." + i);
+  sum = sum+i;
+}
+
+age = 13;
+let r = Date("2017-06-23T12:00:00-09:45");
+console.log(r);
+if (age <= 9) {
+  console.log("lower primary" + age);
+  text("lower primary" + age, 300, 100)
+
+} else if (age <= 12) {
+  console.log("upper primary" + age);
+  text("upper primary" + age, 300, 100);
+
+}else {
+  console.log("secondary school" + age);
+  text("secondary school" + age, 300, 100);
+}
+let i = 19;
+while (i >= 1){
+  console.log(i);
+  i -= 2;
+}
+}
+
+for i in Range(3):\
+
+for(let i = 1, i <= 10, i++){i = 11
+  i = 10
+  .
+  .//code
+}
+
+
 
 function draw(){
   fill(255, 0, 0); //rgb
@@ -34,9 +105,11 @@ function draw(){
   // fill(5, 255, 155);
   // ellipse(300, 300, 100, 100);
 
-  ellipse(100, 100, 30, 50);
-  ellipse(200, 100, 80, 150);
+  // ellipse(100, 100, 30, 50);
+  // ellipse(200, 100, 80, 150);
 
-  circle(320, 120, 100);
-  rect(30, 20, 55, 50, 0, 0, 20, 0)
+  // circle(320, 120, 100);
+  // rect(30, 20, 55, 50, 0, 0, 20, 0)
+  
+
 }
