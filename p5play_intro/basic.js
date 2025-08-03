e46af37790b1a020ce6eaa31d1c7064d6ba60469
ambertan345 <ambertan2011@yahoo.com.sg>
2025-08-03 03:08:25 +0000

    Aug 3, 2025, 3:08 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -22,63 +22,66 @@ function setup() {
   // createCanvas(600, 400);
   // background(220);
 
-// console.log("Hello!");
-// textSize(16);
-// fill(0);
+  // console.log("Hello!");
+  // textSize(16);
+  // fill(0);
 
-// let a = 5;
-// let b = 10;
-// sum = a + b;
-// let product = a * b;
-// console.log("Sum of a and b: ", sum);
-// console.log("Product of a and b: ". product);
+  // let a = 5;
+  // let b = 10;
+  // sum = a + b;
+  // let product = a * b;
+  // console.log("Sum of a and b: ", sum);
+  // console.log("Product of a and b: ". product);
 
-// base = 10;
-// height = 5;
-// let area = base * height * 0.5;
-// console.log("Area of triangle: ", area);
+  // base = 10;
+  // height = 5;
+  // let area = base * height * 0.5;
+  // console.log("Area of triangle: ", area);
 
-// fill(0);
-// text(area, 100, 100)
+  // fill(0);
+  // text(area, 100, 100)
 
 
-// for (let i = 10, i <= 20; i++) {
-//   console.log("Counting..." + i)
-// }
+  // for (let i = 10, i <= 20; i++) {
+  //   console.log("Counting..." + i)
+  // }
 
-// for (let i = 20, i <= 1; i--) {
-//   console.log("Counting..." + i)
-// }
+  // for (let i = 20, i <= 1; i--) {
+  //   console.log("Counting..." + i)
+  // }
 
-// for (let i = 3, i <= 46; i+3) {
-//   console.log("Counting..." + i)
-// }
+  // for (let i = 3, i <= 46; i+3) {
+  //   console.log("Counting..." + i)
+  // }
 
-// for (let i + 2, i <= 20; i+=2) {
-//   console.log("Counting..." + i);
-//   sum = sum+i;
-// }
+  // for (let i + 2, i <= 20; i+=2) {
+  //   console.log("Counting..." + i);
+  //   sum = sum+i;
+  // }
 
-// age = 13;
-// let r = Date("2017-06-23T12:00:00-09:45");
-// console.log(r);
-// if (age <= 9) {
-//   console.log("lower primary" + age);
-//   text("lower primary" + age, 300, 100)
+  // age = 13;
+  // let r = Date("2017-06-23T12:00:00-09:45");
+  // console.log(r);
+  // if (age <= 9) {
+  //   console.log("lower primary" + age);
+  //   text("lower primary" + age, 300, 100)
 
-// } else if (age <= 12) {
-//   console.log("upper primary" + age);
-//   text("upper primary" + age, 300, 100);
+  // } else if (age <= 12) {
+  //   console.log("upper primary" + age);
+  //   text("upper primary" + age, 300, 100);
 
-// }else {
-//   console.log("secondary school" + age);
-//   text("secondary school" + age, 300, 100);
-// }
-// let i = 19;
-// while (i >= 1){
-//   console.log(i);
-//   i -= 2;
-// }
+  // }else {
+  //   console.log("secondary school" + age);
+  //   text("secondary school" + age, 300, 100);
+  // }
+  // let i = 19;
+  // while (i >= 1){
+  //   console.log(i);
+  //   i -= 2;
+  // }
+
+
+  
 }
 
 // for i in Range(3):\
