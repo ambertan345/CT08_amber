df1a865ae1684c5fc1772260f321f2db572aa353
ambertan345 <ambertan2011@yahoo.com.sg>
2025-08-10 03:06:04 +0000

    Aug 10, 2025, 3:06 AM

---
+++ flappybird/flappybird01.js
@@ -0,0 +1,40 @@
+  // write your codes here
+let bird, floor;
+let flapMidImg, bg, base;
+
+function preload(){
+  flapMidImg = loadimage('../assets/yellowbird-midflap.png');
+  bg = loadImage('../assests/background-day.png');
+  base = loadImage('../assets/base.png');
+}
+
+function setup(){
+  new Canvas(400, 600)
+
+  //bird sprite
+  bird = new Sprite();
+  bird.x = width/2
+  bird.y = 200;
+  bird.width = 30;
+  bird.height = 30;
+  bird.img = flapMidImg
+
+  bird.collider = 'dynamic';
+  bird.mass = 2;
+  bird.drag = 0.02;
+  bird.bounciness = 0.5;
+  world.gravity.y = 10;
+
+  floor = new Sprite();
+  floor.x = 200;
+  floor.y = height - 20;
+  floor.width = 400;
+  floor.height = 125;
+  floor.collider = "static";
+  floor.img = base
+
+}
+
+function draw(){
+  image(bg, 0, 0, width, height);
+}
