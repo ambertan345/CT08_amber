7ce426a8f0bceff9748f3cf83b9958c8fe19e6dc
ambertan345 <ambertan2011@yahoo.com.sg>
2025-08-24 03:14:59 +0000

    Aug 24, 2025, 3:14 AM

--- flappybird/playground/pg06_camscrolling.js
+++ flappybird/playground/pg06_camscrolling.js
@@ -52,7 +52,7 @@ function setup(){
 function draw(){
   image(bg, 0, 0, width, height);
 
-  
+
 
   if ( kb.presses('space') || mouse.presses()) {
     bird.vel.y = -5;
@@ -103,7 +103,7 @@ function draw(){
   text('isMoving: ' + bird.isMoing, 10, 40);
   text('sleeping: ' + bird.sleeping, 10, 60);
 
-  
+  bird.x += 3;
 }
 
 
