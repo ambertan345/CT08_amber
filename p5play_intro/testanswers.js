4191a7270677530ce81d817a3fb2d0280e4debd7
ambertan345 <ambertan2011@yahoo.com.sg>
2025-09-07 03:47:48 +0000

    Sep 7, 2025, 3:47 AM

--- p5play_intro/testanswers.js
+++ p5play_intro/testanswers.js
@@ -14,7 +14,8 @@ function setup() {
     //abc.y = 50;
     world.gravity.y = 1
 
-    floor = new Sprite(300, 200)
+    
+    floor = new Sprite(300, 200, 300, 35, "static")
     floor.width = 300
     floor.height = 35
     floor.y = 20
