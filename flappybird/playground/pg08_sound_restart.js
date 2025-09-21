d500e23e718f30e3ecd9ccfe0b29a96283f6e47a
ambertan345 <ambertan2011@yahoo.com.sg>
2025-09-21 04:04:28 +0000

    Sep 21, 2025, 4:04 AM

--- flappybird/playground/pg08_sound_restart.js
+++ flappybird/playground/pg08_sound_restart.js
@@ -108,7 +108,7 @@ function draw(){
     let birdLeftEdge = bird.x - bird.w/2;
 
     if (pipe.passed == false && pipeRightEdge < bird) {
-      pipe.passed = true
+      pipe.passed = true;
       score++
     }
   }
