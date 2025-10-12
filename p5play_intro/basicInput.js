adeb6f8cc01d89ac2b977ce071ef029b7f9a3e95
ambertan345 <ambertan2011@yahoo.com.sg>
2025-10-12 04:24:43 +0000

    Oct 12, 2025, 4:24 AM

--- p5play_intro/basicInput.js
+++ p5play_intro/basicInput.js
@@ -4,9 +4,9 @@ let userText = "Your Text Here";
 function setup() {
     createCanvas(600, 400);
 
-    inputText = createInput();
-    inputText.position(200, height - 80);
-    inputText.input(updateText);
+    // inputText = createInput();
+    // inputText.position(200, height - 80);
+    // inputText.input(updateText);
 
     colorPicker = createColorPicker("#63c1a6ff");
     colorPicker.position =(120, 32);
