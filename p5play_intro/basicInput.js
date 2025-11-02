3e617eff60f1f43594c17fd52a686dbee0b2512d
ambertan345 <ambertan2011@yahoo.com.sg>
2025-11-02 03:44:37 +0000

    Nov 2, 2025, 3:44 AM

--- p5play_intro/basicInput.js
+++ p5play_intro/basicInput.js
@@ -1,41 +1,41 @@
-// let inputText;
-// let userText = "Your Text Here";
-// let colorPicker;
+let inputText;
+let userText = "Your Text Here";
+let colorPicker;
 
-// function setup() {
-//     createCanvas(600, 400);
+function setup() {
+    createCanvas(600, 400);
 
-//     // inputText = createInput();
-//     // inputText.position(200, height - 80);
-//     // inputText.input(updateText);
-//     inputText = createInput();
-//     inputText.position(width/2 - 80, height - 150);
-//     inputText.input(updateText);
+    // inputText = createInput();
+    // inputText.position(200, height - 80);
+    // inputText.input(updateText);
+    inputText = createInput();
+    inputText.position(width/2 - 80, height - 150);
+    inputText.input(updateText);
 
-//     colorPicker = createColorPicker("#63c1a6ff");
-//     colorPicker.position =(120, 32);
-// }
-// function draw() {
-//     background(colorPicker.value());
+    colorPicker = createColorPicker("#63c1a6ff");
+    colorPicker.position =(120, 32);
+}
+function draw() {
+    background(colorPicker.value());
 
-//     textSize(16);
-//     textAlign(LEFT);
-//     fill(0);
-//     text("Pick a color", 20, 50)
+    textSize(16);
+    textAlign(LEFT);
+    fill(0);
+    text("Pick a color", 20, 50)
 
-//     fill(255);
-//     rect(50, 100, 500, 160, 25);
+    fill(255);
+    rect(50, 100, 500, 160, 25);
 
-//     fill(0);
-//     textSize(28);
-//     textAlign(CENTER, CENTER);
-//     text(userText, width/2, 180);
+    fill(0);
+    textSize(28);
+    textAlign(CENTER, CENTER);
+    text(userText, width/2, 180);
 
-// }
-// function updateText() {
-//     userText = this.value();
-//     // return 
-// }
+}
+function updateText() {
+    userText = this.value();
+    // return 
+}
 
 function setup() {
     createCanvas(200, 200)
