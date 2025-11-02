0c606967c4012848e13ff8d5a350ae0a68a94c13
ambertan345 <ambertan2011@yahoo.com.sg>
2025-11-02 03:15:38 +0000

    Nov 2, 2025, 3:15 AM

--- p5play_intro/basicInput.js
+++ p5play_intro/basicInput.js
@@ -9,7 +9,7 @@ function setup() {
     // inputText.position(200, height - 80);
     // inputText.input(updateText);
     inputText = createInput();
-    inputText.position
+    inputText.position(width/2 - 80, height)
 
     colorPicker = createColorPicker("#63c1a6ff");
     colorPicker.position =(120, 32);
